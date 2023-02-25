507ff782121118a710c8a43d4886ee0560b7db8e
nikoagge <n.aggelidis@m-stat.gr>
2023-02-25 17:21:45 +0200

    Initial Commit

---
+++ RickAndMortyIOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyIOSApp
+//
+//  Created by Nikos Aggelidis on 25/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
